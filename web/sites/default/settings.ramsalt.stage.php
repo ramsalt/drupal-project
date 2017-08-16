9a93d905d1d2674c9fcaa1708295664f9ac511a1
Marlon Saglia <esolitos@gmail.com>
2017-08-16 13:31:55 +0200

    Require the prod settings by default on stage and override what's needed

--- web/sites/default/settings.ramsalt.stage.php
+++ web/sites/default/settings.ramsalt.stage.php
@@ -1,4 +1,6 @@
 <?php
+// Good idea is to include the Production config and override what's needed.
+require_once __DIR__ . "/settings.ramsalt.prod.php";
 
 // Uncomment and specify site folder if you use stage file proxy
 //$config['stage_file_proxy.settings']['origin'] = 'http://{site}'; // no trailing slash
