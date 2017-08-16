a02b7f89c77b7041800ccd258f30ba4d9cdeee06
Marlon Saglia <esolitos@gmail.com>
2017-08-16 13:29:51 +0200

    Improve local settings and add config directory

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,16 +1,16 @@
 <?php
 
-/**
+/*
  * Include Wodby settings if required.
  */
 isset($_SERVER['WODBY_CONF']) && include $_SERVER['WODBY_CONF'] . '/wodby.settings.php';
 
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => DRUPAL_ROOT . '/../config/sync/',
-);
+// Set the Config Sync directory to be outside the drupal root
+$config_directories[CONFIG_SYNC_DIRECTORY] = DRUPAL_ROOT . '/../config/sync/';
 
+/*
+ * Include settings based in the environment.
+ */
 $ENV_TYPE = isset($_SERVER['WODBY_ENVIRONMENT_TYPE']) ? $_SERVER['WODBY_ENVIRONMENT_TYPE'] : 'local';
 $filename = __DIR__ . "/settings.ramsalt.{$ENV_TYPE}.php";
-
-if(file_exists($filename))
-  include_once $filename;
+file_exists($filename) && include_once $filename;
--- web/sites/default/settings.ramsalt.local.php
+++ web/sites/default/settings.ramsalt.local.php
@@ -1,5 +1,8 @@
 <?php
+// Good idea is to include the Production config and override what's needed.
+require_once __DIR__ . "/settings.ramsalt.prod.php";
 
+// Change this if you ain't using docker!
 $is_docker = TRUE;
 $your_email = 'your_username@ramsalt.com';
 
@@ -26,7 +29,7 @@ $db_password  = 'drupal';
 /**                                                             **/
 /*****************************************************************/
 
-if ($real_host ) {
+if ($real_host) {
   $real_host_directory = isset($real_host_directory) ? $real_host_directory : $real_host;
   if (isset($real_host_access) )
     $config['stage_file_proxy.settings']['origin'] = 'http://'.$real_host_access.'@'.$real_host;
@@ -41,19 +44,29 @@ $config['system.site']['mail'] = $your_email;
 
 $update_free_access = TRUE;
 
-$settings['hash_salt'] = 'UqHbmo0YHN-uYCwhRPnV2nxNNnVOrddPEQwk5U2AXVZiXsjC5A_WDPdxX4F9IIqG2x0FS4wjKA';
-$settings['install_profile'] = 'standard';
+$settings['hash_salt'] = 'some-very-long-random-string-to-be-changed';
 
-if ($is_docker) {
+/*
+ * @TODO: Change the following when SMTP module has ability to reroute/stop
+ *   mails in config
+ */
+// if ($is_docker) {
   $config['smtp.settings']['smtp_port'] = '1025';
   $config['smtp.settings']['smtp_host'] = 'mailhog';
   $config['smtp.settings']['smtp_from'] = $your_email;
-}
-else {
-  $conf['smtp_reroute_address'] = $your_email;
-}
+// }
+// else {
+  // If using SMTP module
+  // $config['smtp.settings']['smtp_reroute_address'] = $your_email;
+  // If using swiftmailer
+  $config['swiftmailer.transport']['transport'] = 'native';
+// }
 
+// Load services definition file.
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+// Verbose logging by design
 $config['system.logging']['error_level'] = 'verbose';
 
 $config['system.performance']['css']['preprocess'] = FALSE;
@@ -61,21 +74,23 @@ $config['system.performance']['js']['preprocess'] = FALSE;
 
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-$settings['extension_discovery_scan_tests'] = TRUE;
-$settings['rebuild_access'] = TRUE;
-$settings['skip_permissions_hardening'] = TRUE;
 
+$settings['extension_discovery_scan_tests'] = TRUE;
 $settings['file_scan_ignore_directories'] = [
   'node_modules',
   'bower_components',
   '.sass-cache',
 ];
 
+$settings['rebuild_access'] = TRUE;
+$settings['skip_permissions_hardening'] = TRUE;
+
 $default_db_host      = 'mariadb';
 $default_db_port      = '3306';
 $default_db_name      = 'drupal';
 $default_db_user      = 'drupal';
 $default_db_password  = 'drupal';
+// Don't check on pass, because some crazy person could have empty pass for mysql
 if (empty($db_user)) {
   $db_user = $default_db_user;
   $db_password = $default_db_password;
